819b9fa7f4386749051623d0ff4855ca3c015e3f
HoangHuuy <hoanghuy020420@gmail.com>
2023-05-12 17:41:21 +0700

    git deploy

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import homeRoute from "../pages/Home/routes";
 import technologyRoute from "pages/Technology/routes";
 import postRoute from "pages/Post/routes";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   ...homeRoute,
   ...technologyRoute,
   ...postRoute,
